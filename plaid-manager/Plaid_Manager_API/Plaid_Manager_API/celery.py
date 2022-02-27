245369e5724700874e619526c8810c969c693a0b
Prudhvi Vuda <prudhvi12042001@gmail.com>
2022-02-27 23:32:15 +0530

    initial commit

---
+++ plaid-manager/Plaid_Manager_API/Plaid_Manager_API/celery.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Plaid_Manager_API.settings')
+app = Celery('Plaid_Manager_API')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
