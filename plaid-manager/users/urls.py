a703418f79d41136ef7cc0fe805022633162501b
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2022-02-27 23:24:37 +0530

    Add files via upload

---
+++ plaid-manager/users/urls.py
@@ -0,0 +1,10 @@
+from .views import RegisterAPI
+from django.urls import path
+from knox import views as knox_views
+from .views import LoginAPI
+
+urlpatterns = [
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutAllView.as_view(), name='logout'),
+]
