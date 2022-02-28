12e65a8be260dc17c1750409f44a923ec137378a
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2022-02-28 11:07:53 +0530

    Update urls.py

--- plaid-manager/Plaid_Manager_API/token_exchange/urls.py
+++ plaid-manager/Plaid_Manager_API/token_exchange/urls.py
@@ -4,15 +4,14 @@ from . import views
 from token_exchange.views import getLinkToken,getAccessToken,getAccounts,getTransactions,getItems
 
 urlpatterns = [
-    #all POST API calls
+    # all POST API calls
     path('link_token/', getLinkToken.as_view(), name='Get-Plaid-Link_Token'),
     path('get_access_token/', getAccessToken.as_view(), name='Plaid-Link-Public_token'),
     
-
-    #all GET API calls
+    # all GET API calls
+    path('home/', views.home, name='Get-Public_token'),
     path('get_items/', getItems.as_view(), name='Get-Items'),
     path('get_accounts/', getAccounts.as_view(), name='Get-Accounts'),
     path('get_transactions/', getTransactions.as_view(), name='Get-Transactions'),
     path('transaction_webhook/', views.transactionWebhook, name='Transaction-Webhook'),
-    path('home/', views.home, name='Get-Public_token'),
 ]
